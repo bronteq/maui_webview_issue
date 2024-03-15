9c61b71a844d9f9571fc58b773ec06011100a343
bronteq <159045969+bronteq@users.noreply.github.com>
2024-03-15 09:43:08 +0100

    Added debug messages for NewPage constructor and destructor

--- MauiAppWebview/NewPage.xaml.cs
+++ MauiAppWebview/NewPage.xaml.cs
@@ -1,9 +1,17 @@
+using System.Diagnostics;
+
 namespace MauiAppWebview;
 
 public partial class NewPage : ContentPage
 {
 	public NewPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        Debug.WriteLine("Constructor for NewPage()");
+    }
+
+    ~NewPage()
+	{
+        Debug.WriteLine("Finalizer for ~NewPage()");
+    }
 }
